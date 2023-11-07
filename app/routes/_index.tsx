da7ffc12a538cb580659048300b5c3390445a543
paulclindo <paulclindo@gmail.com>
2023-11-07 12:22:39 -0500

    add ssr

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,33 +1,29 @@
-import type { MetaFunction } from "@remix-run/cloudflare";
+import { json, type MetaFunction } from "@remix-run/cloudflare";
+import { useLoaderData } from "@remix-run/react";
 
 export const meta: MetaFunction = () => {
-  return [
-    { title: "New Remix App" },
-    { name: "description", content: "Welcome to Remix!" },
-  ];
+  return [{ title: "New Remix App" }, { name: "description", content: "Welcome to Remix!" }];
 };
 
+export async function loader() {
+  return json({ message: "Hello World111!" });
+}
+
 export default function Index() {
+  const data = useLoaderData<typeof loader>();
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      <h1>Welcome to Remix</h1>
+      <h1>Welcome to Remix </h1>
+      <p>{data.message}</p>
       <ul>
         <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
+          <a target="_blank" href="https://remix.run/tutorials/blog" rel="noreferrer">
+            15m Quickstart Blog Tutorial asdasd
           </a>
         </li>
         <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
+          <a target="_blank" href="https://remix.run/tutorials/jokes" rel="noreferrer">
+            Deep Dive Jokes App Tutorial asdasdas as asd
           </a>
         </li>
         <li>
